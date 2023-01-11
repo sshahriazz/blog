55d3e0f5779cc41f9dd2cfb66522456daead236a
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2023-01-11 18:28:36 +0600

    session typing

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,4 +1,5 @@
 import "next-auth/jwt";
+import { DefaultSession } from "next-auth";
 
 // Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
 
@@ -6,5 +7,17 @@ declare module "next-auth/jwt" {
   interface JWT {
     /** The user's role. */
     userRole?: "admin";
+  }
+}
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      address: string;
+    } & DefaultSession["user"];
   }
 }
