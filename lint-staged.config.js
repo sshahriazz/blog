a9b1aeb53c8827728a65fba9105a9f81e988d4f4
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2023-01-25 21:32:32 +0600

    testing lint stage

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,7 +3,7 @@ module.exports = {
   "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
 
   // This will lint and format TypeScript and                                             //JavaScript files
-  "**/*.(ts|tsx|js)": (filenames) => [
+  "**/*.(ts|tsx|js|jsx)": (filenames) => [
     `yarn eslint --fix ${filenames.join(" ")}`,
     `yarn prettier --write ${filenames.join(" ")}`,
   ],
